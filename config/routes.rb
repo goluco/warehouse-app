56bb31429c924bc745bf2d4105ac047d26e1bb61
Luis Felipe <lfmarquesf@gmail.com>
2022-05-16 23:10:34 -0300

    adiciona modelo de produto e associa a fornecedor

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :suppliers
+  resources :product_models, only: [:index, :show]
 end
---
+++ spec/system/user_views_product_models_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe 'Usuário vê modelos de produtos' do
+    it 'a partir do menu' do
+        #Arrange
+        #Act
+        visit root_path
+        within('nav') do
+            click_on 'Modelos de Produtos'
+        end
+        #Assert
+        expect(current_path).to eq(product_models_path)
+    end
+
+    it 'com sucesso' do
+        #Arrange
+        supplier = Supplier.create!(trade_name: 'LF Muambas', corporate_name: 'Luis Felipe', nif: 12345678901234, address: 'Rua dos Bobos, número 0', email: "lfmuamba@email.com", phone_number: '21998754254')
+        ProductModel.create!(name: 'TV 32', weight: 8000, width: 70, height: 45, depth: 10, sku: 'TV32-LFMU-XPTO90', supplier: supplier)
+        ProductModel.create!(name: 'SoundBar 7.1 Surround', weight: 3000, width: 80, height: 15, depth: 20, sku: 'SOU71-LFMU-NOIZ77', supplier: supplier)
+        #Act
+        visit root_path
+        within('nav') do
+            click_on 'Modelos de Produtos'
+        end
+        #Assert
+        expect(page).to have_content('TV 32')
+        expect(page).to have_content('TV32-LFMU-XPTO90')
+        expect(page).to have_content('LF Muambas')
+        expect(page).to have_content('SoundBar 7.1 Surround')
+        expect(page).to have_content('SOU71-LFMU-NOIZ77')
+        expect(page).to have_content('TV 32')
+    end
+
+    it 'e não existem produtos cadastrados' do
+        #Arrange
+        #Act
+        visit root_path
+        click_on 'Modelos de Produtos'
+        #Assert
+        expect(page).to have_content('Não existem produtos cadastrados.')
+    end
+end
