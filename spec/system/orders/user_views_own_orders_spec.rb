74de42fc165cde1bd8c24e006a3c2cbd8955a355
Luis Felipe <lfmarquesf@gmail.com>
2022-06-07 14:43:09 -0300

    adiciona model order_item

--- spec/system/orders/user_views_own_orders_spec.rb
+++ spec/system/orders/user_views_own_orders_spec.rb
@@ -67,4 +67,26 @@ describe 'Usuário vê seus próprios pedidos' do
          expect(current_path).to eq(root_path)
          expect(page).to have_content('Você não tem acesso a este pedido.')
     end
+
+    it 'e vê itens do pedido' do
+        #Arrange
+        supplier = Supplier.create!(trade_name: 'LF Muambas', corporate_name: 'Luis Felipe Marques', nif: 12345678901234, address: 'Rua dos Bobos, número 0', email: "lfmuamba@email.com", phone_number: 21998754254)
+        product_a = ProductModel.create!(name: 'Produto A', weight: 1, height: 10, width: 20, depth: 5, sku: 12345678901234567890, supplier: supplier)
+        product_b = ProductModel.create!(name: 'Produto B', weight: 2, height: 20, width: 10, depth: 6, sku: 23456678901234567890, supplier: supplier)
+        product_c = ProductModel.create!(name: 'Produto C', weight: 4, height: 15, width: 25, depth: 7, sku: 54321678901234567890, supplier: supplier)
+        user = User.create!(name: 'Luis', email: 'luis@email.com', password: 'password')
+        warehouse = Warehouse.create!(name: 'Rio', code: 'RIO', address: 'Endereço', cep: '20000-000', city: 'Rio de Janeiro', area: 10000, description: 'Alguma descrição')
+        order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now)
+        OrderItem.create!(product_model: product_a, order: order, quantity: 20)
+        OrderItem.create!(product_model: product_b, order: order, quantity: 25)
+        #Act
+        login_as(user)
+        visit root_path
+        click_on 'Meus Pedidos'
+        click_on order.code
+        #Assert
+        expect(page).to have_content('Itens do Pedido')
+        expect(page).to have_content('20 x Produto A')
+        expect(page).to have_content('25 x Produto B')
+    end
 end
