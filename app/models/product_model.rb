d38276d76d0bd6e451ab9e03db62a422dbf0e5e7
Luis Felipe <lfmarquesf@gmail.com>
2022-06-09 09:43:11 -0300

    corrige erro de sintaxe no model product_model

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,7 +1,7 @@
 class ProductModel < ApplicationRecord
   belongs_to :supplier
   has_many :order_items
-  has_many :orders, through: order_items
+  has_many :orders, through: :order_items
   validates :name, :weight, :width, :height, :depth, :sku, :supplier, presence: true
   validates :sku, length: { is: 20 }
   validates :sku, uniqueness: true
